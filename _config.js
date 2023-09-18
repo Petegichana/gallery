a227ca508aaeead439265ba02bec54e50fc14545
Petegichana <ptahgich177@gmail.com>
2023-09-18 14:24:14 +0300

    test

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://petegichana:2011417242.Ab@GalleryCluster.mmyd3z6.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://petegichana:2011417242.Ab@GalleryCluster.mmyd3z6.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://petegichana:2011417242.Ab@GalleryCluster.mmyd3z6.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
