02153f89cb6d9ce5c56b575e44707c7daa72b0c4
Petegichana <ptahgich177@gmail.com>
2023-09-18 23:34:23 +0300

    updated _config.js file

--- _config.js
+++ _config.js
@@ -1,6 +1,5 @@
 var config = {}
 
-// Update to have your correct username and password
 config.mongoURI = {
     production: 'mongodb+srv://petegichana:2011417242.Ab@GalleryCluster.mmyd3z6.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://petegichana:2011417242.Ab@GalleryCluster.mmyd3z6.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
